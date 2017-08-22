26904f5bdf73661df22504e565c88996de67ec77
Ericka Emery <ericka@elonandcompany.com>
2017-08-21 22:56:46 -0500

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -5,9 +5,30 @@ function App(props) {
     return (
       <div className="App">
         	<div className="wrap">
-
+	
 			<div className="header">
-
+	
+			<div className="top-header">
+				<div className="top-header-left">
+					<ul>
+						<li className="active"><a href="#">Home</a></li>
+						<li><a href="#">Specials</a></li>
+						<li><a href="#">Delivery</a></li>
+						<li><a href="#">Contact</a></li>
+					</ul>
+				</div>
+				<div className="top-header-right">
+					<ul>
+						<li><a href="#">CURRENCY:</a></li>
+						<li>
+							<select>
+								<option>Dollar</option>
+								<option>Euro</option>
+								<option>Pound</option>
+							</select>
+						</li>
+					</ul>
+				</div>
 				<div className="clear"> </div>
 			</div>
 			<div className="clear"> </div>
@@ -37,12 +58,20 @@ function App(props) {
 					<div className="clear"> </div>
 				</ul>
 			</div>
-
+		
 			</div>
-
-
-
-
+			
+		
+					<div className="image-slider">
+				
+					    <ul className="rslides" id="slider1">
+					      <li><img src="images/slider1.jpg" alt="" /></li>
+					      <li><img src="images/slider3.jpg" alt="" /></li>
+					      <li><img src="images/slider1.jpg" alt="" /></li>
+					    </ul>
+					
+					</div>
+				
 				<div className="content">
 					<div className="products-box">
 					<div className="products">
@@ -128,9 +157,52 @@ function App(props) {
 				</div>
 			</div>
 			<div className="clear"> </div>
-
+			<div className="footer">
+				<div className="section group">
+				<div className="col_1_of_4 span_1_of_4">
+					<h3>INFORMATION</h3>
+					<ul>
+						<li><a href="#">About us</a></li>
+						<li><a href="#">Sitemap</a></li>
+						<li><a href="#">Contact</a></li>
+						<li><a href="#">Terms and conditions</a></li>
+						<li><a href="#">Legal Notice</a></li>
+					</ul>
+				</div>
+				<div className="col_1_of_4 span_1_of_4">
+					<h3>OUR OFFERS</h3>
+					<ul>
+						<li><a href="#">New products</a></li>
+						<li><a href="#">top sellers</a></li>
+						<li><a href="#">Specials</a></li>
+						<li><a href="#">Products</a></li>
+						<li><a href="#">Comments</a></li>
+					</ul>
+				</div>
+				<div className="col_1_of_4 span_1_of_4">
+					<h3>YOUR ACCOUNT</h3>
+					<ul>
+						<li><a href="#">Your Account</a></li>
+						<li><a href="#">Personal info</a></li>
+						<li><a href="#">Prices</a></li>
+						<li><a href="#">Address</a></li>
+						<li><a href="#">Locations</a></li>
+					</ul>
+				</div>
+				<div className="col_1_of_4 span_1_of_4 footer-lastgrid">
+					<h3>Get in touch</h3>
+					<ul>
+						<li><a href="#"><img src="images/facebook.png" title="facebook" /></a></li>
+						<li><a href="#"><img src="images/twitter.png" title="Twiiter" /></a></li>
+						<li><a href="#"><img src="images/rss.png" title="Rss" /></a></li>
+						<li><a href="#"><img src="images/gpluse.png" title="Google+" /></a></li>
+					</ul>
+					<p>Design by <a href="#">W3layouts</a></p>
+				</div>
+			</div>
+			</div>
 		</div>
-
+	
       </div>
     );
 }
